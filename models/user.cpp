424af24e17c9e5beb3941e45308326dc78f6345e
antonvoznia <antoncty@gmail.com>
2019-08-12 12:50:01 +0200

    created new model

---
+++ models/user.cpp
@@ -0,0 +1,5 @@
+//
+// Created by anton on 8/12/19.
+//
+
+#include "user.h"
---
+++ models/user.h
@@ -0,0 +1,14 @@
+//
+// Created by anton on 8/12/19.
+//
+
+#ifndef CURRENCY_USER_H
+#define CURRENCY_USER_H
+
+
+class user {
+
+};
+
+
+#endif //CURRENCY_USER_H
